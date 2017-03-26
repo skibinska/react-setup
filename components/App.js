76dd09f7c201de7234dcc50c0b91bc17fc00d1c2
ewelina <skibinska.ewelina@gmail.com>
2017-03-26 23:00:21 +0100

    Add basic setup

---
+++ components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+function App () {
+  return (
+    <div>
+      <h1>Hello Ewelina!</h1>
+      <h2>Danon, can you see?</h2>
+    </div>
+  );
+}
+
+module.exports = App;
